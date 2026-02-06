c738ce3004851c18d99e5909826fbd18687f3476
tayogunsola-lab <tayogunsola@gmail.com>
2026-02-06 10:47:13 +0100

    Update index.js with ES6 syntax

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express';
+
 const app = express();
+
 app.get('/', (req, res) => {
-  res.send('✅ Server is running on port 3000');
+  res.send('Server is running on port 3000');
 });
+
 app.listen(3000);
