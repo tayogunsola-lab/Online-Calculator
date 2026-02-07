ba65f5b006bbdf06377fa3e2e3cb409a1d3e778f
Timothy Ayogu <ayogutimothy107@gmail.com>
2026-02-07 15:07:34 +0100

    added the parsing feature

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-import express from 'express';
-import calculator from './calculator.js'
+import express from "express";
+import calculator from "./calculator.js";
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Server is running on port 3000');
+app.get("/calc", (req, res) => {
+  const { num1, num2, op } = req.query;
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is running on port 3000");
 });
 
 app.listen(3000);
